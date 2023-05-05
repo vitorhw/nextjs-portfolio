8836187913b5d42847b6716060b71020ef240d13
vitorhw <vitor.hw@outlook.com>
2023-05-04 21:55:50 -0300

    fix: next Link persisting scroll position

--- app/projects/article.tsx
+++ app/projects/article.tsx
@@ -11,36 +11,32 @@ type Props = {
 
 export const Article: React.FC<Props> = ({ project, views }) => {
   return (
-    <Suspense fallback={<Loading />}>
-      <Link href={`/projects/${project.slug}`}>
-        <article className="p-4 md:p-8">
-          <div className="flex justify-between gap-2 items-center">
-            <span className="text-xs duration-1000 text-gray-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
-              {project.date ? (
-                <time dateTime={new Date(project.date).toISOString()}>
-                  {Intl.DateTimeFormat(undefined, {
-                    dateStyle: "medium",
-                  }).format(new Date(project.date))}
-                </time>
-              ) : (
-                <span>SOON</span>
-              )}
-            </span>
-            <span className="text-gray-150 text-xs  flex items-center gap-1">
-              <Eye className="w-4 h-4" />{" "}
-              {Intl.NumberFormat("en-US", { notation: "compact" }).format(
-                views
-              )}
-            </span>
-          </div>
-          <h2 className="z-20 mt-2 text-xl font-medium duration-1000 lg:text-3xl text-gray-200 group-hover:text-white font-display">
-            {project.title}
-          </h2>
-          <p className="z-20 mt-4 text-sm  duration-1000 text-gray-150 group-hover:text-gray-200">
-            {project.description}
-          </p>
-        </article>
-      </Link>
-    </Suspense>
+    <Link href={`/projects/${project.slug}`} scroll={false}>
+      <article className="p-4 md:p-8">
+        <div className="flex justify-between gap-2 items-center">
+          <span className="text-xs duration-1000 text-gray-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
+            {project.date ? (
+              <time dateTime={new Date(project.date).toISOString()}>
+                {Intl.DateTimeFormat(undefined, {
+                  dateStyle: "medium",
+                }).format(new Date(project.date))}
+              </time>
+            ) : (
+              <span>SOON</span>
+            )}
+          </span>
+          <span className="text-gray-150 text-xs  flex items-center gap-1">
+            <Eye className="w-4 h-4" />{" "}
+            {Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
+          </span>
+        </div>
+        <h2 className="z-20 mt-2 text-xl font-medium duration-1000 lg:text-3xl text-gray-200 group-hover:text-white font-display">
+          {project.title}
+        </h2>
+        <p className="z-20 mt-4 text-sm  duration-1000 text-gray-150 group-hover:text-gray-200">
+          {project.description}
+        </p>
+      </article>
+    </Link>
   );
 };
